a8adb56bd2eedf600d4807f03d6b2bd082bc70b5
Neil Yonzon <neilyonzon@Lubbys-Laptop.lan>
2021-01-28 02:13:34 -0800

    Added Design and Toggle Functionality for Header Profile Nav. Notes added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "gatsby";
 import Logo from "../Logo";
 import ProfileIcon from "./Profile";
 
@@ -7,8 +6,7 @@ const Header = () => {
   return (
     <header className="main-header">
       <Logo addClass=" main-header__logo"></Logo>
-
-      {/* Conditionally render below if authorized/profile exists */}
+      {/* Conditionally render below after authorization/User exists */}
       <ProfileIcon></ProfileIcon>
     </header>
   );
--- src/components/header/Profile.js
+++ src/components/header/Profile.js
@@ -1,29 +1,61 @@
 import React from "react";
 import { Link } from "gatsby";
+import Placeholder from "../../images/profile-image-placeholder.png";
 
 class ProfileIcon extends React.Component {
   state = {
-    displayProfile: " header-profile__nav--hide",
+    displayProfile: " ",
+  };
+
+  // Notes. How to make 'detect click outside element' a universal/helper function accessible for all components?
+
+  componentDidMount() {
+    document.addEventListener("mousedown", this.hideProfileNav);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.hideProfileNav);
+  }
+
+  setWrapperRef = (node) => {
+    this.wrapperRef = node;
+  };
+
+  hideProfileNav = (event) => {
+    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+      if (this.state.displayProfile == " header-profile__nav--visible") {
+        this.setState({
+          displayProfile: " ",
+        });
+      }
+    }
   };
 
   toggleProfileNav = () => {
-    this.state.displayProfile === " header-profile__nav--hide"
+    this.state.displayProfile === " header-profile__nav--visible"
       ? this.setState({ displayProfile: "" })
-      : this.setState({ displayProfile: " header-profile__nav--hide" });
+      : this.setState({ displayProfile: " header-profile__nav--visible" });
   };
 
   render() {
     return (
-      <div className="header-profile">
-        <img
-          src="https://via.placeholder.com/150"
-          className="header-profile__img"
-          alt="placeholder"
+      // 1. API call to display image. Use placeholder if no image.
+      // 2. Logout to Expire authentication and redirect to Login | Sign-Up Home Page View.
+      // 3. Profile to route to profile page. Profile design/page tbd.
+      <div className="header-profile" ref={this.setWrapperRef}>
+        <div
+          class="header-profile__img-container"
           onClick={this.toggleProfileNav}
-        ></img>
+        >
+          <img
+            src={Placeholder}
+            className="header-profile__img"
+            alt="placeholder"
+          ></img>
+        </div>
         <ul className={"header-profile__nav" + this.state.displayProfile}>
           <li className="header-profile__nav-item">
-            <Link activeClassName="header-profile__nav-link" to="/">
+            <Link activeClassName="header-profile__nav-link" to="/about">
               Profile
             </Link>
           </li>
