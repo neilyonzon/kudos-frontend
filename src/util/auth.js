aad24e21bd0e31c70d9322898a982d02759c25c5
kitakoj18 <kitakoj18@gmail.com>
2021-01-28 21:00:20 -0800

    started working on auth util file
    "

---
+++ src/util/auth.js
@@ -0,0 +1,35 @@
+export const isBrowser = () => typeof window !== "undefined";
+
+export const getUser = () => 
+    window.localStorage.getItem("userInfo") 
+        ? JSON.parse(window.localStorage.getItem("userInfo"))
+        : {}
+
+const setUser = userInfo => (window.localstorage.setItem("userInfo", JSON.stringify(userInfo)));
+
+export const loginUser = ({ username, password }) =>{
+    if(!isBrowser){
+        return false;
+    }
+
+    const graphqlQuery = {
+        query: `
+        {
+            loginTeacher(teacherInput: {username: "${username}", password:"${password}"}) {
+              token
+            }
+        }
+      `
+    }
+
+    let userData;
+    fetch('http://localhost:3000/graphql', {
+        method: 'POST',
+        body: JSON.stringify(graphqlQuery)
+    }).then(response => {
+        return response.json();
+    }).then(data =>{
+        userData = data;
+    }).catch(error => console.log(error));
+
+}
