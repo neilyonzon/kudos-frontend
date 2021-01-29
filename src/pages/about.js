474009777c5753b9fcad2d322f4a5d9311f50bf1
Neil Yonzon <neilyonzon@Lubbys-Laptop.lan>
2021-01-28 19:50:58 -0800

    Updated Header CSS

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,6 @@
 import React from "react";
 import Layout from "../components/Layout";
+
 export default function About() {
-  return (
-    <Layout>
-      <div style={{ color: `teal` }}>
-        <h1>About Gatsby</h1>
-        <p>Such wow. Very React.</p>
-      </div>
-    </Layout>
-  );
+  return <Layout></Layout>;
 }
