57baa8a3ebb84db39a33515bf6371c7e0b83084f
kitakoj18 <kitakoj18@gmail.com>
2021-01-31 13:57:34 -0800

    wrapped dummy component in PrivateRoute to protect route from users who are not logged in

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Router } from '@reach/router'
 import Layout from '../components/Layout'
 import Login from '../components/Login'
 import Dummy from '../components/Dummy'
+import PrivateRoute from '../components/PrivateRoute'
 
 // markup
 const IndexPage = () => {
@@ -13,8 +14,8 @@ const IndexPage = () => {
         <meta name="description" content="Kudos" />
       </Helmet>
       <Router>
-        <Login path="/"/>
-        <Dummy path="/dummy"/>
+        <Login path="/" />
+        <PrivateRoute path='/dummy' component={Dummy} />
       </Router>
     </Layout>
   );
