2fbc94093d10c9488fc8cb34267b1b54013371db
kitakoj18 <kitakoj18@gmail.com>
2021-01-31 13:29:41 -0800

    created dummy and login pages

---
+++ src/components/Dummy.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Dummy extends Component {
+    render(){
+        return (
+            <h1>This is the dummy page!</h1>
+        )
+    }
+}
+
+export default Dummy;
