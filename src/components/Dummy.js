dfd30657caacd3248977e95a2b33445f9a00bfdd
kitakoj18 <kitakoj18@gmail.com>
2021-01-31 13:57:56 -0800

    added log out button

--- src/components/Dummy.js
+++ src/components/Dummy.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
+import { logout } from '../utils/auth'
+import { navigate } from 'gatsby';
 
 class Dummy extends Component {
     render(){
         return (
-            <h1>This is the dummy page!</h1>
+            <div>
+                <h1>This is the dummy page!</h1>
+                <a
+                    href="/"
+                    onClick={event =>{
+                        event.preventDefault();
+                        logout(() => navigate('/'))
+                    }}
+                >
+                    Log Out!
+                </a>
+            </div>
+
         )
     }
 }
