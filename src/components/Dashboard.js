f0e5e659ebfb97208ab7b65dc5e5489e3c194193
kitakoj18 <kitakoj18@gmail.com>
2021-02-03 16:29:14 -0800

    imported data from SidebarIconsData to map elements to li

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react'
 import { logout } from '../utils/auth'
 import { navigate } from 'gatsby'
 
-import { FaAward } from '@react-icons/all-files/fa/FaAward'
-import { IoIosHome } from '@react-icons/all-files/io/IoIosHome'
-import { IoIosPeople } from '@react-icons/all-files/io/IoIosPeople'
-import { IoIosSettings } from '@react-icons/all-files/io/IoIosSettings'
+import { SidebarIconsData } from './dashboard/SidebarIconsData'
 
 class Dashboard extends Component {
     render(){
@@ -13,10 +10,17 @@ class Dashboard extends Component {
             <div>
                 <h1>This is the dashboard page!</h1>
 
-                <IoIosHome />
-                <IoIosPeople />
-                <FaAward />
-                <IoIosSettings />
+                <nav>
+                    <ul>
+                        {SidebarIconsData.map(iconData =>{
+                            return (
+                                <li key={iconData.component}>
+                                    {iconData.icon}
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </nav>
 
                 <a
                     href="/"
