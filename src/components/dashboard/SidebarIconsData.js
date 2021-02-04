f1a5e8ceb32abf6d69a251a66c31438c573c21c0
kitakoj18 <kitakoj18@gmail.com>
2021-02-03 17:08:41 -0800

    readded SidebarIconsData.js

---
+++ src/components/dashboard/SidebarIconsData.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { FaAward } from '@react-icons/all-files/fa/FaAward'
+import { IoIosHome } from '@react-icons/all-files/io/IoIosHome'
+import { IoIosPeople } from '@react-icons/all-files/io/IoIosPeople'
+import { IoIosSettings } from '@react-icons/all-files/io/IoIosSettings'
+
+export const SidebarIconsData = [
+    {
+        component: 'Home',
+        icon: <IoIosHome />,
+    },
+    {
+        component: 'Students',
+        icon: <IoIosPeople />,
+    },
+    {
+        component: 'Award',
+        icon: <FaAward />,
+    },
+    {
+        component: 'Settings',
+        icon: <IoIosSettings />,
+    },
+]
