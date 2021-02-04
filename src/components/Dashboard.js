f61ab2d624787036b27a2ff2f0f4daec07d63e22
kitakoj18 <kitakoj18@gmail.com>
2021-02-03 19:59:49 -0800

    Linked Home, Students, and TreasureBox components and added onComponentSelectHandler to change components when pressing associated icon

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,12 +2,37 @@ import React, { Component } from 'react'
 import { logout } from '../utils/auth'
 import { navigate } from 'gatsby'
 
+import Home from './Home'
+import Students from './Students'
+import TreasureBox from './TreasureBox'
+
 import dashboardStyles from './dashboard.module.css'
 
 import { SidebarIconsData } from './dashboard/SidebarIconsData'
 
 class Dashboard extends Component {
+
+    state = {
+        selectedComponentName: 'Home'
+    }
+
+    onComponentSelectHandler = (selectedComponentName) =>{
+        this.setState({selectedComponentName: selectedComponentName})
+    }
+
     render(){
+
+        let selectedComponent
+        if(this.state.selectedComponentName === 'Home'){
+            selectedComponent = <Home />
+        }
+        if(this.state.selectedComponentName === 'Students'){
+            selectedComponent = <Students />
+        }
+        if(this.state.selectedComponentName === 'TreasureBox'){
+            selectedComponent = <TreasureBox />
+        }
+
         return (
             <div>
                 <h1>This is the dashboard page!</h1>
@@ -26,13 +51,20 @@ class Dashboard extends Component {
                     <ul className={dashboardStyles.navMenuItems}>
                         {SidebarIconsData.map(iconData =>{
                             return (
-                                <li key={iconData.component} className={dashboardStyles.navText}>
+                                <li 
+                                    key={iconData.component} 
+                                    className={dashboardStyles.navText}
+                                    onClick={() => this.onComponentSelectHandler(iconData.component)}>
                                     {iconData.icon}
                                 </li>
                             )
                         })}
                     </ul>
                 </nav>
+
+                <h1>Below is the selected component</h1>
+
+                {selectedComponent}
 
             </div>
 
