ec3e7eaeb5eb353a1ab969cadeda556ed63c1116
Neil Yonzon <neilyonzon@Lubbys-Laptop.lan>
2021-02-03 20:37:55 -0800

    Added static html and css for Login page

---
+++ src/pages/login.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { Helmet } from "react-helmet";
+import Layout from "../components/Layout";
+import ToyBoyImg from "../../src/images/toy-box.png";
+
+const LoginPage = () => {
+  return (
+    <Layout>
+      <Helmet>
+        <title>Kudos Login</title>
+        <meta name="description" content="Kudos" />
+      </Helmet>
+      <div className="bg bg--yellow">
+        <div className="login-panel">
+          <div className="login-panel__content">
+            <h1>Manage Prizes</h1>
+            <p>Organize and save time with our prize management tool.</p>
+            <button className="btn btn-primary">Login</button>
+            <button className="btn btn-primary--inv">Login</button>
+          </div>
+          <div className="login-panel__line"></div>
+          <div className="login-panel__img">
+            <img src={ToyBoyImg} alt="Toy Box Illustration"></img>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default LoginPage;
