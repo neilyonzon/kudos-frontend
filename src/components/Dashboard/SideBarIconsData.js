4f9dc5f8dd4f32fd2fcd322c4b71179846688322
kitakoj18 <kitakoj18@gmail.com>
2021-02-03 16:30:09 -0800

    changed filename

--- src/components/Dashboard/SideBarIconsData.js
+++ src/components/Dashboard/SideBarIconsData.js
@@ -5,7 +5,7 @@ import { IoIosHome } from '@react-icons/all-files/io/IoIosHome'
 import { IoIosPeople } from '@react-icons/all-files/io/IoIosPeople'
 import { IoIosSettings } from '@react-icons/all-files/io/IoIosSettings'
 
-export const SideBarIconsData = [
+export const SidebarIconsData = [
     {
         component: 'Home',
         icon: <IoIosHome />,
