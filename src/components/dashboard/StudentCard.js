55c84d41800192e5b5556fc297494ffb15bfea1c
kitakoj18 <kitakoj18@gmail.com>
2021-02-05 14:51:17 -0800

    created studentCard to display for each student on dashboard

---
+++ src/components/dashboard/StudentCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FiDollarSign } from '@react-icons/all-files/fi/FiDollarSign'
+import { HiPencilAlt } from '@react-icons/all-files/hi/HiPencilAlt'
+
+const StudentCard = props => {
+    return (
+        <div>
+            <h1>{props.name}</h1>
+            <h2>{props.kudosBalance}</h2>
+            <FiDollarSign />
+            <HiPencilAlt />
+        </div>
+    )
+};
+
+export default StudentCard;
