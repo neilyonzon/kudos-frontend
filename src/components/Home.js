cf8fc73392d77c317f6000e4dc065762fac4e33f
kitakoj18 <kitakoj18@gmail.com>
2021-02-05 14:59:00 -0800

    added StudentCard component to take in props of all students and output card for each

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import StudentCard from './dashboard/StudentCard'
 
 class Home extends Component {
     
     render(){
+        console.log('meeeeep')
+        console.log(this.props.data)
+
         return (
-            <h1>This is the home component</h1>
+            <div>
+                <h1>This is the home component</h1>
+                {this.props.data[0].students.map(student => {
+                    return <StudentCard name={student.firstName} kudosBalance={student.kudosBalance}/>
+                })}
+            </div>
         )
     }
 }
