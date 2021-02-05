f01790878aaec5c8abcf10a0338c7b8d3ac42d60
kitakoj18 <kitakoj18@gmail.com>
2021-02-05 15:27:30 -0800

    initialized css for studentCard

--- src/components/dashboard/StudentCard.js
+++ src/components/dashboard/StudentCard.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { FiDollarSign } from '@react-icons/all-files/fi/FiDollarSign'
 import { HiPencilAlt } from '@react-icons/all-files/hi/HiPencilAlt'
 
+import studentCardStyles from './studentCard.module.css'
+
 const StudentCard = props => {
     return (
         <div>
