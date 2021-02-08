be1dd77852fcc71765e0e51fc8c96f57dc218eae
kitakoj18 <kitakoj18@gmail.com>
2021-02-07 17:35:00 -0800

    started styling TBSummary component

--- src/components/dashboard/TBSummary.js
+++ src/components/dashboard/TBSummary.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import tbSummaryStyling from './tbSummary.module.css'
+
+import { FaMoneyBillAlt } from '@react-icons/all-files/fa/FaMoneyBillAlt'
+
 const TBSummary = props =>{
 
     return(
         <div>
+            <div className={ tbSummaryStyling.moneyIcon }>
+                <FaMoneyBillAlt />
+            </div>
             <div>
                 {props.remainingPrizes} remaining
             </div>
