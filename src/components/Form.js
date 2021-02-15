e63cb1efe6f8f39053787e40cbd69a470b548912
Neil Yonzon <neilyonzon@Lubbys-Laptop.lan>
2021-02-15 00:05:41 -0800

    Added input

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,6 +6,8 @@ class Form extends Component {
   render() {
     return (
       <div>
+        <h1>Get Started</h1>
+        <p>Lorem ipsum dolor sit amet, consecte</p>
         <form className="form" id="example-form">
           <div className="form__group">
             <label className="form__label--hidden" htmlFor="username">
@@ -58,6 +60,7 @@ class Form extends Component {
               name="desecription"
               id="description"
               rows="4"
+              placeholder="Type Description..."
             ></textarea>
             <div
               id="helper-text__description"
@@ -68,14 +71,42 @@ class Form extends Component {
             </div>
           </div>
           <div className="form__group">
-            <label className="form__label" htmlFor="category">
+            <label className="form__label--hidden" htmlFor="category">
               Category
             </label>
-            <select className="form__input" name="category" id="category">
+            <select className="form__select" name="category" id="category">
+              <option value="default" disabled hidden>
+                Categories
+              </option>
               <option value="arts">Arts &amp; Crafts</option>
               <option value="games">Games</option>
               <option value="toys">Toys</option>
             </select>
+          </div>
+          <div className="form__group">
+            <div className="form__radio-group">
+              <input type="radio" id="option1" name="options" value="option1" />
+              <label htmlFor="option1" className="form__label">
+                Option 1
+              </label>
+            </div>
+            <div className="form__radio-group">
+              <input type="radio" id="option2" name="options" value="option2" />
+              <label htmlFor="option2" className="form__label">
+                Option 2
+              </label>
+            </div>
+            <div className="form__radio-group">
+              <input
+                type="radio"
+                id="option3"
+                name="options"
+                value="option3"
+              ></input>
+              <label htmlFor="option3" className="form__label">
+                Option 3
+              </label>
+            </div>
           </div>
         </form>
         <button className="btn btn-primary">Call to Action</button>
