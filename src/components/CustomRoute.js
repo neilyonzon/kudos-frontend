bd5b09c9054f9f9521c1eacdde3f5c8722d87c75
kitakoj18 <kitakoj18@gmail.com>
2021-03-08 22:36:34 -0800

    adjusted custom component so that it can await async loggedIn function

--- src/components/CustomRoute.js
+++ src/components/CustomRoute.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { navigate } from 'gatsby'
-import { isLoggedIn } from '../utils/auth'
+import { isLoggedIn, setAcsToken } from '../utils/auth'
 
 const CustomRoute = ({ component: Component, location, ...rest }) =>{
 
+    const [isUserLoggedIn, setIsUserLoggedIn] = useState(null)
+
+    useEffect(() => {
+        async function getLoginStatus(){
+            const loggedIn = await isLoggedIn()
+            setIsUserLoggedIn(loggedIn)
+        }
+        getLoginStatus()
+    }, [])
+
     // if user goes to protected route but isn't logged in, go to home page (which is the login page)
-    if(!isLoggedIn() && location.pathname !== '/'){
+    if(!isUserLoggedIn && location.pathname !== '/'){
         navigate('/')
         return null
     }
 
     // if user goes to home page but is logged in, go straight to dashboard
-    if(isLoggedIn() && location.pathname === '/'){
+    if(isUserLoggedIn && location.pathname === '/'){
         navigate('/dashboard')
         return null
     }
