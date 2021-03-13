115210aa01e782eb64ec907bb8e56a6387b71855
kitakoj18 <kitakoj18@gmail.com>
2021-03-12 16:39:20 -0800

    initialized apollo client and wrapped app around apollo provider

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,19 +6,28 @@ import Dashboard from "../components/Dashboard";
 import { Helmet } from "react-helmet";
 import "../sass/style.scss";
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  cache: new InMemoryCache()
+})
+
 // markup
 const IndexPage = () => {
   return (
-    <Layout>
-      <Helmet>
-        <title>Kudos Login</title>
-        <meta name="description" content="Kudos" />
-      </Helmet>
-      <Router>
-        <Login path="/" />
-        <Dashboard path="/dashboard" />
-      </Router>
-    </Layout>
+    <ApolloProvider client={client}>
+      <Layout>
+        <Helmet>
+          <title>Kudos Login</title>
+          <meta name="description" content="Kudos" />
+        </Helmet>
+        <Router>
+          <Login path="/" />
+          <Dashboard path="/dashboard" />
+        </Router>
+      </Layout>
+    </ApolloProvider>
   );
 };
 
