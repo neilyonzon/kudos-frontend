4039a308489dfac3f8fbbebc23891fb8e9f55ca0
kitakoj18 <kitakoj18@gmail.com>
2021-03-13 19:47:11 -0800

    moved credentials into link configuration

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,9 +4,9 @@ import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from '@apollo/c
 const client = new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-        uri: 'http://localhost:3000/graphql'
-    }),
-    credentials: 'include'
+        uri: 'http://localhost:3000/graphql',
+        credentials: 'include'
+    })
 })
 
 export const wrapRootElement = ({ element }) =>(
