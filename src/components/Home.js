efb05f01623cf58a98cf9dad76cf72d06852f73f
kitakoj18 <kitakoj18@gmail.com>
2021-04-14 17:47:24 -0600

    added pending approval line items

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import { gql, useQuery } from '@apollo/client'
 
 import StudentCard from './dashboard/StudentCard'
 import TBSummary from './dashboard/TBSummary'
+import Approval from './treasureBox/Approval'
 
 const Home = props =>{
 
@@ -22,6 +23,7 @@ const Home = props =>{
                         id
                         approved
                         prizeId
+                        prizeName
                         prizeCost
                         prizeImageUrl
                     }
@@ -63,11 +65,14 @@ const Home = props =>{
     // }
 
     const classStudents = data.getClassInfo.students
-    let pendingApproval = 0
+    let numPendingApproval = 0
+    let pendingApprovals = []
     for(const student of classStudents){
         for(const transaction of student.transactions){
             if(!transaction.approved){
-                pendingApproval += 1
+                numPendingApproval += 1
+                const studentName = student.firstName + ' ' + student.lastName
+                pendingApprovals.push(<Approval key={transaction.id} studentName={studentName} prizeCost={transaction.prizeCost} prizeName={transaction.prizeName}/>)
             }
         }
     }
@@ -80,11 +85,12 @@ const Home = props =>{
     return(
         <div>
             <h1>The home component is selected</h1>
-            <h3>{pendingApproval} waiting to be approved</h3>
+            <h3>{numPendingApproval} waiting to be approved</h3>
             <h3>{remainingPrizes} prizes remain</h3>
             {classStudents.map(student => {
                 return <StudentCard key={student.id} name={student.firstName} kudosBalance={student.kudosBalance} />
             })}
+            {pendingApprovals}
         </div>
     )
 }
