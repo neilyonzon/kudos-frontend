b319e2ebeac320d13c72eada7d159a4ab2ead31b
kitakoj18 <kitakoj18@gmail.com>
2021-04-19 20:42:59 -0600

    moved logo into global folder

--- src/components/global/Header.js
+++ src/components/global/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../Logo";
+import Logo from "./Logo";
 import ProfileIcon from "./Profile";
 import { loginSuccesssful, isLoggedIn } from "../../utils/auth";
 
