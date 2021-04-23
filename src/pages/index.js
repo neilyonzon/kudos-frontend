a3650ad1c20d4c19d01cd6a633b397bc8e4ebb03
kitakoj18 <kitakoj18@gmail.com>
2021-04-22 18:34:46 -0600

    changed home page to home component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Router } from "@reach/router";
 import Layout from "../components/global/Layout";
 import Welcome from "./welcome";
-import Dashboard from "../components/Dashboard";
+import Home from "./Home";
 import { Helmet } from "react-helmet";
 import "../sass/style.scss";
 
@@ -16,7 +16,7 @@ const IndexPage = () => {
       </Helmet>
       <Router>
         <Welcome path="/" />
-        <Dashboard path="/dashboard" />
+        <Home path="/dashboard" />
       </Router>
     </Layout>
   );
