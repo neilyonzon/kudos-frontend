d7bd32de5cbb7790bcf157ad0dd21ab4cc7a798f
kitakoj18 <kitakoj18@gmail.com>
2021-06-01 21:40:34 -0700

    added new image utility function to send form data to backend to retrieve imageUrl

--- src/utils/image.js
+++ src/utils/image.js
@@ -1,3 +1,5 @@
+import { retrieveAcsToken } from './auth'
+
 export const generateImageBase64 = imageFile => {
     const reader = new FileReader()
     const promise = new Promise((resolve, reject) => {
@@ -7,4 +9,17 @@ export const generateImageBase64 = imageFile => {
 
     reader.readAsDataURL(imageFile)
     return promise
+}
+
+export const postImage = async (formData) => {
+    const acsToken = await retrieveAcsToken()
+    const response = await fetch('http://localhost:3000/post-image', {
+        method: 'PUT',
+        headers: {
+            Authorization: 'Bearer ' + acsToken 
+        },
+        body: formData
+    })
+    const responseData = await response.json()
+    return responseData
 }
