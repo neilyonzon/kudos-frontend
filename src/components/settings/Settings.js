87826d9516119e419f1ebc6a2b1332553dfabd86
kitakoj18 <kitakoj18@gmail.com>
2021-08-03 21:50:29 -0700

    refactored so that only selected settings tab is rendered at one time

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -5,12 +5,14 @@ import CategoriesForm from "./CategoriesForm";
 import ClassesForm from "./ClassesForm";
 
 const Settings = (props) => {
+
+  const [selectedSetting, setSelectedSetting] = useState('general')
+
   const [tabClasses, setTabClasses] = useState([
     { tabClass: "tabs__header active", contentClass: "tabs__content active" },
     { tabClass: "tabs__header", contentClass: "tabs__content" },
     { tabClass: "tabs__header", contentClass: "tabs__content" },
   ]);
-
 
   const handleTabChange = (index) => {
     let tabs = JSON.parse(JSON.stringify(tabClasses));
@@ -25,7 +27,28 @@ const Settings = (props) => {
       contentClass: "tabs__content active",
     };
     setTabClasses(tabs);
+
+    if(index === 0){
+      setSelectedSetting('general')
+    } else if(index === 1){
+      setSelectedSetting('classes')
+    } else {
+      setSelectedSetting('categories')
+    }
   };
+
+  let selectedSettingComponent
+  switch (selectedSetting) {
+    case 'general':
+      selectedSettingComponent = <GeneralForm loadUserInfo={props.loadUserInfo} teacherInfo={props.data.teacher}/>
+      break
+    case 'classes':
+      selectedSettingComponent = <ClassesForm classes={props.data.teacher.classes} />
+      break
+    case 'categories':
+      selectedSettingComponent = <CategoriesForm loadUserInfo={props.loadUserInfo} categories={props.categories} />
+      break
+  }
 
   return (
     <>
@@ -55,15 +78,18 @@ const Settings = (props) => {
           </div>
         </div>
         <div className="tabs__content-container">
-          <div className={tabClasses[0].contentClass} id="tabcontent-1">
+          <div className={"tabs__content active"}>
+            {selectedSettingComponent}
+          </div>
+          {/* <div className={tabClasses[0].contentClass} id="tabcontent-1">
             <GeneralForm loadUserInfo={props.loadUserInfo} teacherInfo={props.data.teacher}/>
           </div>
           <div className={tabClasses[1].contentClass} id="tabcontent-2">
             <ClassesForm refreshData={props.loadUserInfo} classes={props.data.teacher.classes} />
           </div>
           <div className={tabClasses[2].contentClass} id="tabcontent-3">
-            <CategoriesForm />
-          </div>
+            <CategoriesForm loadUserInfo={props.loadUserInfo} categories={props.categories} />
+          </div> */}
         </div>
       </div>
     </>
