7c9500ea470e5f79495a5f5ea77be9e8c46b704b
kitakoj18 <kitakoj18@gmail.com>
2021-08-11 17:35:27 -0700

    created editcategory card component to help with mapping categories and applying deletion

---
+++ src/components/settings/EditCategoryCard.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import DeleteCatModal from './DeleteCatModal'
+
+const EditCategoryCard = props => {
+
+    const [deleteCatModalOpen, setDeleteCatModalOpen] = useState(false)
+
+    const openDeleteClassModal = () => setDeleteCatModalOpen(true)
+    const closeDeleteClassModal = () => setDeleteCatModalOpen(false)
+
+    return (
+        <div className="categories-settings__group">
+            <label className="settings-form__label">
+                {props.item.category === "Toy" ? `${props.item.category} (Default)` : props.item.category}
+            </label>
+            
+            {props.item.category !== "Toy" ? 
+            <>
+                <input
+                    type="text"
+                    className="settings-form__input-text"
+                    id={props.item.category}
+                    value={props.item.category}
+                />
+                <button onClick={openDeleteClassModal}>Delete</button>
+            </>
+            : null
+            }
+            <DeleteCatModal 
+                isOpen={deleteCatModalOpen}
+                onClose={closeDeleteClassModal}
+                toDeleteCategory={props.item}
+                categories={props.categories}
+                loadUserInfo={props.loadUserInfo}
+            />
+        </div>
+    )
+}
+
+export default EditCategoryCard
